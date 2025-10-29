922c01dd4654064eeed693f30b9d0bee57fab123
NeroClaudia <itzfaraways10@gmail.com>
2025-10-29 17:40:11 +0700

    Menambahkan berapa mahasiswa yang lulus dan tidak

--- SiakadFor07.java
+++ SiakadFor07.java
@@ -1,22 +1,31 @@
 import java.util.Scanner;
+
 public class SiakadFor07 {
     public static void main(String[] args) {
         
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-    double nilai, tertinggi = 0, terendah = 100;
+        double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
-    for (int i = 1; i <= 10; i++) {
-        System.out.print("Masukkan Nilai Mahasiswa ke-" + i + ": ");
-        nilai = sc.nextDouble();
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan Nilai Mahasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
 
-        if (nilai > tertinggi)
-            tertinggi = nilai;
+            if (nilai > tertinggi)
+                tertinggi = nilai;
             if (nilai < terendah)
-            terendah = nilai;
-    }
-    System.out.println("Nilai Tertinggi: " + tertinggi);
-    System.out.println("Nilai Terendah: " + terendah);
-    sc.close();
+                terendah = nilai;
+
+            if (nilai >= 60)
+                lulus++;
+            else
+                tidakLulus++;
+        }
+        System.out.println("Nilai Tertinggi: " + tertinggi);
+        System.out.println("Nilai Terendah: " + terendah);
+        System.out.println("Jumlah Mahasiswa Lulus: " + lulus);
+        System.out.println("Jumlah Mahasiswa Tidak Lulus: " + tidakLulus);
+        sc.close();
     }
 }
